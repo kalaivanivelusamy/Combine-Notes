80504268a2fb7ce7ab14e38b75bea4e415b2bc18
KalaivaniVelusamy <kalai.v.vani@gmail.com>
2021-11-30 22:28:52 +0530

    added filter test cases

--- Combine-NotesTests/Combine_NotesTests.swift
+++ Combine-NotesTests/Combine_NotesTests.swift
@@ -78,217 +78,246 @@ class Combine_NotesTests: XCTestCase {
         XCTAssertNotNil(cancellable)
     }
     
-    func testDataTaskPublisher() {
+    func testFilter() {
         
-        let expectation = XCTestExpectation(description: "Fetching from url \(String(describing: testUrlString))")
+        let simplePub = PassthroughSubject<String, Error>()
         
-        let remoteDataPublisher = URLSession.shared.dataTaskPublisher(for: self.testURL!)
+        let cancellable = simplePub
+            .filter {val in
+                return val == "bull's eye"
+            }
+            .print(self.debugDescription)
             .sink(receiveCompletion: { compln in
+                print(".sink() received the completion:", String(describing: compln))
                 switch compln {
-                    case .finished:
-                        expectation.fulfill()
-                    case .failure:
-                        XCTFail()
-                }
-                
-            }, receiveValue: { (data,response) in
-                XCTAssertNotNil(data)
-            })
-        
-        XCTAssertNotNil(remoteDataPublisher)
-        wait(for: [expectation], timeout: 5.0)
-    }
-    
-    func testDataDecodePipeline() {
-        let expectation = XCTestExpectation(description: testUrlString)
-        
-        let dataPublisher = URLSession.shared.dataTaskPublisher(for: testURL!)
-            .map{$0.data}
-            .decode(type: TodoTask.self, decoder: JSONDecoder())
-            .subscribe(on: self.myBackgroundQueue!)
-            .eraseToAnyPublisher()
-        
-        XCTAssertNotNil(dataPublisher)
-        
-        let cancellable = dataPublisher.sink(receiveCompletion: { compln in
-            print(".sink() received the completion", String(describing: compln))
-            switch compln {
-                case .finished:
-                    expectation.fulfill()
-                case .failure:
-                    XCTFail()
-            }
-            
-        }, receiveValue: { data in
-            XCTAssertNotNil(data)
-            print(".sink received some value: \(data)")
-            
-        })
-        
-        wait(for: [expectation], timeout: 5.0)
-        XCTAssertNotNil(cancellable)
-        
-    }
-    
-    func testFailingURLDecodePipeline_URLError() {
-        let myURL = URL(string: "https://urldonoexist.com")
-        let expectation = XCTestExpectation(description: "Download from \(String(describing: myURL))")
-        
-        let taskPub = URLSession.shared.dataTaskPublisher(for: myURL!)
-            .map {$0.data}
-            .decode(type: TodoTask.self, decoder: JSONDecoder())
-            .subscribe(on: self.myBackgroundQueue!)
-            .eraseToAnyPublisher()
-        
-        XCTAssertNotNil(taskPub)
-        
-        let cancellable = taskPub.sink(receiveCompletion: { compln in
-            switch compln {
-                case .finished:
-                    XCTFail()
-                case .failure:
-                    expectation.fulfill()
-            }
-            
-        }, receiveValue: { vsl in
-            XCTFail("should not have received value with the failed url")
-            print("sink received some val \(vsl)")
-        })
-        
-        XCTAssertNotNil(cancellable)
-        wait(for: [expectation], timeout: 5.0)
-    }
-    
-    func testDataTaskPublisherWithTryMap() {
-        
-        let expectation = XCTestExpectation(description: "Download from \(testUrlString)")
-        
-        let taskpub = URLSession.shared.dataTaskPublisher(for: self.testURL!)
-            .tryMap { data,response -> Data in
-                guard let httpResponse = response as? HTTPURLResponse,httpResponse.statusCode == 200 else {
-                    throw TestFailureCondition.invalidServerResponse
-                }
-                return data
-            }
-            .decode(type: TodoTask.self, decoder: JSONDecoder())
-            .subscribe(on: self.myBackgroundQueue!)
-            .eraseToAnyPublisher()
-        
-        XCTAssertNotNil(taskpub)
-        
-        let cancellable = taskpub.sink(receiveCompletion: { compln in
-            switch compln {
-                case .finished:
-                    expectation.fulfill()
-                case .failure(let msg):
-                    XCTFail(msg.localizedDescription)
-            }
-            
-        }, receiveValue: { decodedResponse in
-            XCTAssertNotNil(decodedResponse)
-            XCTAssertTrue(decodedResponse.completed)
-        })
-        XCTAssertNotNil(cancellable)
-        wait(for: [expectation], timeout: 5.0)
-        
-    }
-    
-    func testURL404NotFound() {
-        
-        let expectation = XCTestExpectation(description: "URL not found")
-        let taskPublisher = URLSession.shared.dataTaskPublisher(for: URL(string: test404UrlString)!)
-            .sink(receiveCompletion: {compln in
-                switch compln {
-                    case .finished:
-                        break
-                    case .failure(let error):
-                        print("Received error \(error)")
-                }
-                expectation.fulfill()
-            }, receiveValue: { data , response in
-                guard let response = response as? HTTPURLResponse else{
-                    XCTFail("unable to parse response")
-                    return
-                }
-                
-                let stringData = String(data: data, encoding: .utf8)
-                print(".sink data received \(data) as \(String(describing: stringData))")
-                print("http response received \(response)")
-            })
-        XCTAssertNotNil(taskPublisher)
-        wait(for: [expectation], timeout: 5.0)
-        
-    }
-    
-    func testMax() {
-        
-        let passSubj = PassthroughSubject<Int,Error>()
-        var latestReceivedValue: Int?
-        let cancellable = passSubj
-            .max()
-            .sink(receiveCompletion: { compln in
-                switch compln {
-                    case .finished:
-                        break
                     case .failure(let anError):
-                        print("Received Error \(anError)")
+                        print(".sink() received completion error: ", anError)
+                        XCTFail("no error should be received")
                         break
-                }
-                
-            }, receiveValue: { responseValue in
-                print(".sink() data received \(responseValue)")
-                latestReceivedValue = responseValue
-            })
-        passSubj.send(1)
-        XCTAssertNil(latestReceivedValue)
-        passSubj.send(2)
-        XCTAssertNil(latestReceivedValue)
-        passSubj.send(completion: Subscribers.Completion.finished)
-        XCTAssertEqual(latestReceivedValue, 2)
-        passSubj.send(3)
-        print("latest value \(latestReceivedValue!)")
-        XCTAssertEqual(latestReceivedValue, 2)
-        XCTAssertNotNil(cancellable)
-
-    }
-    
-    struct ExampleStruct{
-        var property1: Int
-        var property2: Int?
-    }
-    
-    func testMaxWithClosure() {
-        
-        let passSubj = PassthroughSubject<ExampleStruct,Error>()
-        var latestValue: ExampleStruct?
-        
-        let cancellable = passSubj
-            .max{ (struct1, struct2) -> Bool in 
-                return struct1.property1 < struct2.property1
-            }
-            .sink(receiveCompletion: { compln in 
-                switch compln {
                     case .finished:
                         break
-                    case .failure(let err):
-                        print("Error caused \(err)")
-                        break
                 }
-            }, receiveValue: { value in
-                print(".sink received value")
-                latestValue = value
-            })
-        
-        passSubj.send(ExampleStruct(property1: 12, property2: 2))
-        XCTAssertNil(latestValue)
-        passSubj.send(ExampleStruct(property1: 2, property2: 23))
-        XCTAssertNil(latestValue)
-        passSubj.send(completion: Subscribers.Completion.finished)
-        XCTAssertEqual(latestValue?.property1, 12)
-        XCTAssertEqual(latestValue?.property2, 2)
-        XCTAssertNotNil(cancellable)
-
-    }
-    
-    
-}
+                }, receiveValue: { stringValue in
+                    print(".sink() received \(stringValue)")
+                    XCTAssertEqual(stringValue, "bull's eye")
+                })
+                simplePub.send("bull's eye")
+                simplePub.send("bull's")
+                simplePub.send(completion: Subscribers.Completion.finished)
+                XCTAssertNotNil(cancellable)
+            }
+                  
+                  func testDataTaskPublisher() {
+                
+                let expectation = XCTestExpectation(description: "Fetching from url \(String(describing: testUrlString))")
+                
+                let remoteDataPublisher = URLSession.shared.dataTaskPublisher(for: self.testURL!)
+                    .sink(receiveCompletion: { compln in
+                        switch compln {
+                            case .finished:
+                                expectation.fulfill()
+                            case .failure:
+                                XCTFail()
+                        }
+                        
+                    }, receiveValue: { (data,response) in
+                        XCTAssertNotNil(data)
+                    })
+                
+                XCTAssertNotNil(remoteDataPublisher)
+                wait(for: [expectation], timeout: 5.0)
+            }
+                  
+                  func testDataDecodePipeline() {
+                let expectation = XCTestExpectation(description: testUrlString)
+                
+                let dataPublisher = URLSession.shared.dataTaskPublisher(for: testURL!)
+                    .map{$0.data}
+                    .decode(type: TodoTask.self, decoder: JSONDecoder())
+                    .subscribe(on: self.myBackgroundQueue!)
+                    .eraseToAnyPublisher()
+                
+                XCTAssertNotNil(dataPublisher)
+                
+                let cancellable = dataPublisher.sink(receiveCompletion: { compln in
+                    print(".sink() received the completion", String(describing: compln))
+                    switch compln {
+                        case .finished:
+                            expectation.fulfill()
+                        case .failure:
+                            XCTFail()
+                    }
+                    
+                }, receiveValue: { data in
+                    XCTAssertNotNil(data)
+                    print(".sink received some value: \(data)")
+                    
+                })
+                
+                wait(for: [expectation], timeout: 5.0)
+                XCTAssertNotNil(cancellable)
+                
+            }
+                  
+                  func testFailingURLDecodePipeline_URLError() {
+                let myURL = URL(string: "https://urldonoexist.com")
+                let expectation = XCTestExpectation(description: "Download from \(String(describing: myURL))")
+                
+                let taskPub = URLSession.shared.dataTaskPublisher(for: myURL!)
+                    .map {$0.data}
+                    .decode(type: TodoTask.self, decoder: JSONDecoder())
+                    .subscribe(on: self.myBackgroundQueue!)
+                    .eraseToAnyPublisher()
+                
+                XCTAssertNotNil(taskPub)
+                
+                let cancellable = taskPub.sink(receiveCompletion: { compln in
+                    switch compln {
+                        case .finished:
+                            XCTFail()
+                        case .failure:
+                            expectation.fulfill()
+                    }
+                    
+                }, receiveValue: { vsl in
+                    XCTFail("should not have received value with the failed url")
+                    print("sink received some val \(vsl)")
+                })
+                
+                XCTAssertNotNil(cancellable)
+                wait(for: [expectation], timeout: 5.0)
+            }
+                  
+                  func testDataTaskPublisherWithTryMap() {
+                
+                let expectation = XCTestExpectation(description: "Download from \(testUrlString)")
+                
+                let taskpub = URLSession.shared.dataTaskPublisher(for: self.testURL!)
+                    .tryMap { data,response -> Data in
+                        guard let httpResponse = response as? HTTPURLResponse,httpResponse.statusCode == 200 else {
+                            throw TestFailureCondition.invalidServerResponse
+                        }
+                        return data
+                    }
+                    .decode(type: TodoTask.self, decoder: JSONDecoder())
+                    .subscribe(on: self.myBackgroundQueue!)
+                    .eraseToAnyPublisher()
+                
+                XCTAssertNotNil(taskpub)
+                
+                let cancellable = taskpub.sink(receiveCompletion: { compln in
+                    switch compln {
+                        case .finished:
+                            expectation.fulfill()
+                        case .failure(let msg):
+                            XCTFail(msg.localizedDescription)
+                    }
+                    
+                }, receiveValue: { decodedResponse in
+                    XCTAssertNotNil(decodedResponse)
+                    XCTAssertTrue(decodedResponse.completed)
+                })
+                XCTAssertNotNil(cancellable)
+                wait(for: [expectation], timeout: 5.0)
+                
+            }
+                  
+                  func testURL404NotFound() {
+                
+                let expectation = XCTestExpectation(description: "URL not found")
+                let taskPublisher = URLSession.shared.dataTaskPublisher(for: URL(string: test404UrlString)!)
+                    .sink(receiveCompletion: {compln in
+                        switch compln {
+                            case .finished:
+                                break
+                            case .failure(let error):
+                                print("Received error \(error)")
+                        }
+                        expectation.fulfill()
+                    }, receiveValue: { data , response in
+                        guard let response = response as? HTTPURLResponse else{
+                            XCTFail("unable to parse response")
+                            return
+                        }
+                        
+                        let stringData = String(data: data, encoding: .utf8)
+                        print(".sink data received \(data) as \(String(describing: stringData))")
+                        print("http response received \(response)")
+                    })
+                XCTAssertNotNil(taskPublisher)
+                wait(for: [expectation], timeout: 5.0)
+                
+            }
+                  
+                  func testMax() {
+                
+                let passSubj = PassthroughSubject<Int,Error>()
+                var latestReceivedValue: Int?
+                let cancellable = passSubj
+                    .max()
+                    .sink(receiveCompletion: { compln in
+                        switch compln {
+                            case .finished:
+                                break
+                            case .failure(let anError):
+                                print("Received Error \(anError)")
+                                break
+                        }
+                        
+                    }, receiveValue: { responseValue in
+                        print(".sink() data received \(responseValue)")
+                        latestReceivedValue = responseValue
+                    })
+                passSubj.send(1)
+                XCTAssertNil(latestReceivedValue)
+                passSubj.send(2)
+                XCTAssertNil(latestReceivedValue)
+                passSubj.send(completion: Subscribers.Completion.finished)
+                XCTAssertEqual(latestReceivedValue, 2)
+                passSubj.send(3)
+                print("latest value \(latestReceivedValue!)")
+                XCTAssertEqual(latestReceivedValue, 2)
+                XCTAssertNotNil(cancellable)
+                
+            }
+                  
+                  struct ExampleStruct{
+                var property1: Int
+                var property2: Int?
+            }
+                  
+                  func testMaxWithClosure() {
+                
+                let passSubj = PassthroughSubject<ExampleStruct,Error>()
+                var latestValue: ExampleStruct?
+                
+                let cancellable = passSubj
+                    .max{ (struct1, struct2) -> Bool in 
+                        return struct1.property1 < struct2.property1
+                    }
+                    .sink(receiveCompletion: { compln in 
+                        switch compln {
+                            case .finished:
+                                break
+                            case .failure(let err):
+                                print("Error caused \(err)")
+                                break
+                        }
+                    }, receiveValue: { value in
+                        print(".sink received value")
+                        latestValue = value
+                    })
+                
+                passSubj.send(ExampleStruct(property1: 12, property2: 2))
+                XCTAssertNil(latestValue)
+                passSubj.send(ExampleStruct(property1: 2, property2: 23))
+                XCTAssertNil(latestValue)
+                passSubj.send(completion: Subscribers.Completion.finished)
+                XCTAssertEqual(latestValue?.property1, 12)
+                XCTAssertEqual(latestValue?.property2, 2)
+                XCTAssertNotNil(cancellable)
+                
+            }
+                  
+                  
+                  }
